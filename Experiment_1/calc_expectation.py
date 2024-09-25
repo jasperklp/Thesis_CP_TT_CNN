ab88624f63c614ddf1dfe63405fffc646479ca54
jasperklp <jasperklip2@gmail.com>
2024-09-25 11:50:57 +0200

    Changed list to tuple type and added chaching

--- Experiment_1/calc_expectation.py
+++ Experiment_1/calc_expectation.py
@@ -1,4 +1,5 @@
 import math
+import functools
 
 #Defaults
 
@@ -8,38 +9,39 @@ bits_per_element_default = 32 #Gives the default number of bits for a memeory op
 
 
 
-#Checks whether the input is an integer or a list of 2 ints. If it is an integer it returns a list of two. If it's neither of both then raises exception.
+#Checks whether the input is an integer or a tuple of 2 ints. If it is an integer it returns a tuple of two ints. If it's neither of both then raises exception.
 #Input:     The value of the variable to be checked
 #           The name of that variable
-#Output:    A list containing of two ints with the size of the variable.
-
-def check_int_or_list_of_int(value_to_check, name_of_value_to_check):
-    if (type(value_to_check) is int):
-        return [value_to_check, value_to_check]
-    elif (type(value_to_check) is list):
-        if not(any(not isinstance(x,int) for x in value_to_check) | (len(value_to_check) != 2)) :
+#Output:    A tuple containing of two ints with the size of the variable.
+@functools.lru_cache
+def check_int_or_tuple_of_int(value_to_check : int | tuple, name_of_value_to_check : str):  
+    if (isinstance(value_to_check, int)):
+        return (value_to_check, value_to_check)
+    elif (isinstance(value_to_check, tuple)) :
+          if ((len(value_to_check) == 2) & (all(isinstance(i,int) for i in value_to_check))):
             return value_to_check
-    raise ValueError(f'{name_of_value_to_check} should either be a single int or a list of two ints')
+    raise ValueError(f'{name_of_value_to_check} should either be a single int or a tuple of two ints')
 
 #Calculates the dimensions of the output image from the input image.
-#The inputs should be given as list of two ints or as a single int this case it will be used for both input dimensions.
+#The inputs should be given a tuple of two ints or as a single int this case it will be used for both input dimensions.
 #Input:         image       : This are the dimensions of the input image
 #               stride      : This is the stride of the CNN
 #               padding     : This is the padding of the CNN
 #               dilitation  : This is the dilitation of the CNN
 #               k
-def calc_output_image_dim(kernel_size : list, stride : list, padding : list, dilitation: list, in_image: list):
+@functools.lru_cache
+def calc_output_image_dim(kernel_size : tuple, stride : tuple, padding : tuple, dilitation: tuple, in_image: tuple):
     image_out = []
     for i in range(2):
             image_out.append(math.floor((in_image[i]  + 2 * padding[i] - dilitation[i] * (kernel_size[i] - 1) - 1)/stride[i]+1))
     return image_out
 
 def validate_MAC_or_RAM_calc_input(kernel_size, stride, padding, dilitation, image, method, rank):
-    kernel_size = check_int_or_list_of_int(kernel_size,   "kernel_size")
-    stride      = check_int_or_list_of_int(stride,        "stride")
-    padding     = check_int_or_list_of_int(padding,       "padding")
-    dilitation  = check_int_or_list_of_int(dilitation,    "dilitation")
-    image       = check_int_or_list_of_int(image,         "image") 
+    kernel_size = check_int_or_tuple_of_int(kernel_size,   "kernel_size")
+    stride      = check_int_or_tuple_of_int(stride,        "stride")
+    padding     = check_int_or_tuple_of_int(padding,       "padding")
+    dilitation  = check_int_or_tuple_of_int(dilitation,    "dilitation")
+    image       = check_int_or_tuple_of_int(image,         "image") 
 
     if (method in {'cp','tucker','tt'}) & (rank == None):
         raise ValueError(f'For {method} rank cannot be None\nPlease insert a rank')
@@ -51,18 +53,18 @@ def validate_MAC_or_RAM_calc_input(kernel_size, stride, padding, dilitation, ima
 #Input:         in_channel      : Number of in_channels
 #               out_channel     : Number of out_channels
 #               kernel_size     : Kernel size of the CNN layer, could be square or single integer
-#               image           : Size of the image. This is assumed to be 2D. Input can be a single integer or a list of two ints.
+#               image           : Size of the image. This is assumed to be 2D. Input can be a single integer or a tuple of two ints.
 #               method          : Method which is used inside the CNN layer. This could be uncomp (for uncompressed), cp (for canoncial polyadic), tucker (for tucker) or tt for tensor train
-#               stride          : This is the stride used in the CNN. This could be a list of two ints or a single integer which is applied to both sides.
-#               padding         : Give the padding of the CNN. This could be a List of two ints or one int which is applied to both sides of the input image.
-#               dilitation      : Gives the dilitation of the CNN. This could be a list of two ints or one int which is applied to both sizes of the input image.
+#               stride          : This is the stride used in the CNN. This could be a tuple of two ints or a single integer which is applied to both sides.
+#               padding         : Give the padding of the CNN. This could be a tuple of two ints or one int which is applied to both sides of the input image.
+#               dilitation      : Gives the dilitation of the CNN. This could be a tuple of two ints or one int which is applied to both sizes of the input image.
 #kwargs
-#               rank            : Defaults ('None') user is required to give a (list of) ranks for the cp, tucker and tt decomposition. For uncompressed this is ignored.
+#               rank            : Defaults ('None') user is required to give a (tuple of) ranks for the cp, tucker and tt decomposition. For uncompressed this is ignored.
 #               bits_per_element: States the number of bits in memory for each element. Defaults to 32 (for 32 bit floating point), but can be adjusted.
-def ram_estimation_2d(in_channel : int, out_channel : int, kernel_size, image, method, stride, padding, dilitation, rank=None, bits_per_element : int = bits_per_element_default):
-    #Check input parameters which could ether be an int or a list of ints.
+def ram_estimation_2d(in_channel : int, out_channel : int, kernel_size : int | tuple[int,int], image: int | tuple[int,int], method: int | tuple[int,int], stride: int | tuple[int,int], padding: int | tuple[int,int], dilitation: int | tuple[int,int], rank=None, bits_per_element : int = bits_per_element_default):
+    #Check input parameters which could ether be an int or a tuple of ints.
     [kernel_size, stride, padding, dilitation, image] = validate_MAC_or_RAM_calc_input(kernel_size, stride, padding, dilitation, image, method, rank)
-    
+    print(1)
     #Calculate the output image as it will always have the same shape
     image_out = calc_output_image_dim(kernel_size,stride, padding, dilitation, image)
     
@@ -87,7 +89,7 @@ def ram_estimation_2d(in_channel : int, out_channel : int, kernel_size, image, m
         kernal_storage_size.append(kernel_size[1] * rank)
         kernal_storage_size.append(out_channel * rank)
         
-        #Insert the storage sizes of the consecutive filter images in a list
+        #Insert the storage sizes of the consecutive filter images in a tuple
         filter_storage_size.append(image[0] * image[1] * rank)
         filter_storage_size.append(image[0] * image_out[1] * rank)
         filter_storage_size.append(image_out[0] * image_out[1] * rank)
@@ -108,14 +110,14 @@ def ram_estimation_2d(in_channel : int, out_channel : int, kernel_size, image, m
 #Input:         in_channel      : Number of in_channels
 #               out_channel     : Number of out_channels
 #               kernel_size     : Kernel size of the CNN layer, could be square or single integer
-#               image           : Size of the image. This is assumed to be 2D. Input can be a single integer or a list of two ints.
+#               image           : Size of the image. This is assumed to be 2D. Input can be a single integer or a tuple of two ints.
 #               method          : Method which is used inside the CNN layer. This could be uncomp (for uncompressed), cp (for canoncial polyadic), tucker (for tucker) or tt for tensor train
-#               stride          : This is the stride used in the CNN. This could be a list of two ints or a single integer which is applied to both sides.
-#               padding         : Give the padding of the CNN. This could be a List of two ints or one int which is applied to both sides of the input image.
-#               dilitation      : Gives the dilitation of the CNN. This could be a list of two ints or one int which is applied to both sizes of the input image.
+#               stride          : This is the stride used in the CNN. This could be a tuple of two ints or a single integer which is applied to both sides.
+#               padding         : Give the padding of the CNN. This could be a tuple of two ints or one int which is applied to both sides of the input image.
+#               dilitation      : Gives the dilitation of the CNN. This could be a tuple of two ints or one int which is applied to both sizes of the input image.
 
-def MAC_estimation_2d(in_channel : int, out_channel : int, kernel_size, image, method, stride, padding, dilitation, rank=None):
-    #Check input parameters which could ether be an int or a list of ints.
+def MAC_estimation_2d(in_channel : int, out_channel : int, kernel_size: int | tuple[int,int], image: int | tuple[int,int], method: int | tuple[int,int], stride: int | tuple[int,int], padding: int | tuple[int,int], dilitation: int | tuple[int,int], rank=None):
+    #Check input parameters which could ether be an int or a tuple of ints.
     [kernel_size, stride, padding, dilitation, image] = validate_MAC_or_RAM_calc_input(kernel_size, stride, padding, dilitation, image, method, rank)
     
     #Calculate the output image as it will always have the same shape
@@ -144,11 +146,11 @@ def MAC_estimation_2d(in_channel : int, out_channel : int, kernel_size, image, m
 
 #This function passes trough a lot of variables MAC and RAM estimation for CNN's have in common. For an explanation of the inputs see reqested functions.
 
-def MAC_and_ram_estimation_2d(in_channel : int, out_channel : int, kernel_size, image, method, stride, padding, dilitation, rank=None, bits_per_element : int = bits_per_element_default):
+def MAC_and_ram_estimation_2d(in_channel : int, out_channel : int, kernel_size, image: int | tuple[int,int], method: int | tuple[int,int], stride: int | tuple[int,int], padding: int | tuple[int,int], dilitation: int | tuple[int,int], rank=None, bits_per_element : int = bits_per_element_default):
     [kernel_size, stride, padding, dilitation, image] = validate_MAC_or_RAM_calc_input(kernel_size, stride, padding, dilitation, image, method, rank)
     
-    MAC = MAC_estimation_2d(in_channel, out_channel, kernel_size, image, method, stride, padding, dilitation, rank=rank, bits_per_element=bits_per_element)
-    RAM = ram_estimation_2d(in_channel, out_channel, kernel_size, image, method, stride, padding, dilitation, rank=rank)
+    MAC = MAC_estimation_2d(in_channel, out_channel, kernel_size, image, method, stride, padding, dilitation, rank=rank)
+    RAM = ram_estimation_2d(in_channel, out_channel, kernel_size, image, method, stride, padding, dilitation, rank=rank, bits_per_element=bits_per_element)
 
     return [MAC, RAM]
     
--- test/Experiment_1/test_calc_expectation.py
+++ test/Experiment_1/test_calc_expectation.py
@@ -1,17 +1,16 @@
 import pytest
 from Experiment_1 import calc_expectation
 
-
 #Test whether al input ar inserted correctly
 def test_check_input_int():
-    assert calc_expectation.check_int_or_list_of_int(3,'kernel_size') == [3,3]
+    assert calc_expectation.check_int_or_list_of_int(3,'kernel_size') == (3,3)
 
 def test_check_input_list_of_2_int():
-    assert calc_expectation.check_int_or_list_of_int([3,3],'kernel_size') == [3,3]
+    assert calc_expectation.check_int_or_list_of_int((3,3),'kernel_size') == (3,3)
 
 def test_check_input_list_of_3_int():
     with pytest.raises(ValueError, match=r".* should either be a single int or a list of two ints"):
-        calc_expectation.check_int_or_list_of_int([3,3,3],'kernel_size')
+        calc_expectation.check_int_or_list_of_int((3,3,3),'kernel_size')
 
 def test_check_input_type_float():
     with pytest.raises(ValueError, match=r".* should either be a single int or a list of two ints"):
@@ -20,48 +19,53 @@ def test_check_input_type_float():
 #Check whether rank is required
 def test_rank_requirement():
     with pytest.raises(ValueError, match=r".* rank cannot be None\nPlease insert a rank"):
-        calc_expectation.ram_estimation_2d(10,20,[3,3],[30,40], 'cp' , 1,1,1)
+        calc_expectation.ram_estimation_2d(10,20,(3,3),(30,40), 'cp' , 1,1,1)
 
 
 # Test outcomes memory uncomp
 def test_uncomp_same_input_output_image_size():
-    assert calc_expectation.ram_estimation_2d(10,20,3,[30,40], 'uncomp' , 1,1,1) == 1209600
+    assert calc_expectation.ram_estimation_2d(10,20,3,(30,40), 'uncomp' , 1,1,1) == 1209600
 
 def test_uncomp_dif_input_output_image_size():
-    assert calc_expectation.ram_estimation_2d(10,20,5,[30,40], 'uncomp' , 1,1,1) == 38280 * 32
+    assert calc_expectation.ram_estimation_2d(10,20,5,(30,40), 'uncomp' , 1,1,1) == 38280 * 32
 
 def test_uncomp_no_square_kernel():
-    assert calc_expectation.ram_estimation_2d(10,20,[3,5],[30,40], 'uncomp' , 1,1,1) == 37800 * 32
+    assert calc_expectation.ram_estimation_2d(10,20,(3,5),(30,40), 'uncomp' , 1,1,1) == 37800 * 32
 
 
 # Test outcomes memory CP
 def test_cp_same_input_output_image_size():
-    assert calc_expectation.ram_estimation_2d(10,20,3,[30,40], 'cp', 1,1,1,rank = 5) == 54180 * 32 # nr of elements times number of bits.
+    assert calc_expectation.ram_estimation_2d(10,20,3,(30,40), 'cp', 1,1,1,rank = 5) == 54180 * 32 # nr of elements times number of bits.
 
 def test_cp_dif_input_output_image_size():
-    assert calc_expectation.ram_estimation_2d(10,20,5,[30,40], 'cp', 1,1,1,rank = 5) == 50500 * 32
+    assert calc_expectation.ram_estimation_2d(10,20,5,(30,40), 'cp', 1,1,1,rank = 5) == 50500 * 32
 
 def test_cp_no_square_kernel():
-    assert calc_expectation.ram_estimation_2d(10,20,[3,5],[30,40], 'cp' , 1,1,1,rank = 5) == 52390 * 32
+    assert calc_expectation.ram_estimation_2d(10,20,(3,5),(30,40), 'cp' , 1,1,1,rank = 5) == 52390 * 32
     
 
 # Test outcomes MAC 
 def test_MAC_uncomp_same_input_output_image_size():
-    assert calc_expectation.MAC_estimation_2d(10,20,3,[30,40], 'uncomp' , 1,1,1) == 3 * 3 * 10 * 20 * 30 * 40
+    assert calc_expectation.MAC_estimation_2d(10,20,3,(30,40), 'uncomp' , 1,1,1) == 3 * 3 * 10 * 20 * 30 * 40
 
 def test_MAC_uncomp_dif_input_output_image_size():
-    assert calc_expectation.MAC_estimation_2d(10,20,5,[30,40], 'uncomp' , 1,1,1) == 5 * 5 * 10 * 20 * 28 * 38
+    assert calc_expectation.MAC_estimation_2d(10,20,5,(30,40), 'uncomp' , 1,1,1) == 5 * 5 * 10 * 20 * 28 * 38
 
 def test_MAC_uncomp_no_square_kernel():
-    assert calc_expectation.MAC_estimation_2d(10,20,[3,5],[30,40], 'uncomp' , 1,1,1) == 3 * 5 * 10 * 20 * 30 * 38 
+    assert calc_expectation.MAC_estimation_2d(10,20,(3,5),(30,40), 'uncomp' , 1,1,1) == 3 * 5 * 10 * 20 * 30 * 38 
 
 # Test outcomes MAC CP
 def test_MAC_cp_same_input_output_image_size():
-    assert calc_expectation.MAC_estimation_2d(10,20,3,[30,40], 'cp', 1,1,1,rank = 5) ==  216000
+    assert calc_expectation.MAC_estimation_2d(10,20,3,(30,40), 'cp', 1,1,1,rank = 5) ==  216000
 def test_MAC_cp_diff_input_output_image_size():
-    assert calc_expectation.MAC_estimation_2d(10,20,5,[30,40], 'cp', 1,1,1,rank = 5) == 221500
+    assert calc_expectation.MAC_estimation_2d(10,20,5,(30,40), 'cp', 1,1,1,rank = 5) == 221500
 def test_MAC_cp_non_square_kernel():
-    assert calc_expectation.MAC_estimation_2d(10,20,[3,5],[30,40], 'cp' , 1,1,1,rank = 5) == 219600
+    assert calc_expectation.MAC_estimation_2d(10,20,(3,5),(30,40), 'cp' , 1,1,1,rank = 5) == 219600
+
+
+#Test combination
+def test_MACRAM_uncomp_dif_no_square_kernel_input_output_image():
+    assert calc_expectation.MAC_and_ram_estimation_2d(10,20,(3,7), (30,40), 'uncomp', 1, 2, 3) == [calc_expectation.MAC_estimation_2d(10,20,(3,7), (30,40), 'uncomp', 1, 2, 3), calc_expectation.ram_estimation_2d(10,20,(3,7), (30,40), 'uncomp', 1, 2, 3)]
 
 
 
