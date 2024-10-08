8247d38dc8519e4a8622d63a838862fcccb36ccb
jasperklp <jasperklip2@gmail.com>
2024-10-08 14:05:20 +0200

    Added the Tensorly Tucker model

--- Experiment_1/CNN_models.py
+++ Experiment_1/CNN_models.py
@@ -96,6 +96,54 @@ class cp_tensorly_model(torch.nn.Module):
         return self.encoder(x)
     
 
+class tucker_tensorly_model(torch.nn.Module):
+    def __init__(self,in_channels   :int
+                 , out_channels     :int
+                 , kernel_size      :int | tuple
+                 , rank             :int | float 
+                 , stride           :int | tuple        = 1
+                 , padding          :int | tuple |str   = 0
+                 , dilation         :int | tuple        = 1
+                 , groups           :int                = 1
+                 , bias             :bool               = True              
+                 , padding_mode     :str                = 'zeros'     
+                 , device           :str                = None            
+                 , dtype            :torch.dtype        = torch.float32
+                 , implementation   :str                = 'factorized'):
+        super().__init__()
+
+        self._in_channels    = in_channels
+        self._out_channels   = out_channels
+        self._kernel_size    = kernel_size
+        self._rank           = rank
+        self._implementation = implementation   
+        self._stride         = stride
+        self._padding        = padding
+        self._dilation       = dilation
+        self._bias           = bias
+        self._padding_mode   = padding_mode
+        self._dtype          = dtype
+
+        self.encoder = tltorch.FactorizedConv.from_conv(torch.nn.Conv2d(in_channels,out_channels,kernel_size,stride,padding,dilation,groups,bias,padding_mode,device,dtype)
+                                                        , rank
+                                                        , implementation=implementation
+                                                        , factorization='tucker'
+                                                        , decompose_weights= False)
+        
+    def MAC_and_RAM(self, image)
+        if self._implementation == 'factorized':
+            method = 'tucker'
+        elif self._implementation == 'reconstructed':
+            method = 'uncomp'
+        else:
+            raise NotImplemented(r'Implementation does not exist for the TuckerConv')
+        
+        return calc_expectation.MAC_and_ram_estimation_2d(self._in_channels, self._out_channels, self._kernel_size, image, method, self._stride, self._padding, self._dilation, bits_per_element=torch.finfo(self._dtype).bits,rank=self._rank)
+        
+    def forward(self,x):
+        return self.encoder(x)
+    
+
 
     
     
