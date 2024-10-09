f131e285c9908dce83341cf6b7982d545a8cbc85
jasperklp <jasperklip2@gmail.com>
2024-10-09 16:38:55 +0200

    Some tracing options are added. Now it is possible to see approximately the correct values.

--- Experiment_1/experiment.py
+++ Experiment_1/experiment.py
@@ -3,13 +3,20 @@ import CNN_models
 
 model_1 = CNN_models.uncomp_model(256,256,3)
 model_2 = CNN_models.cp_tensorly_model(256,256,3,rank=0.1)
+
+image_size_uncomp   = 40
+image_size_cp       = 40
+
+
 if __name__ == "__main__":
-    time = runner.model_runner(model_1, 100 ,4, verbose=True)
-    time = runner.model_runner(model_2, 100, 4, verbose=True)
+    time = runner.model_runner(model_1, 1 ,40, verbose=True)
+    time = runner.model_runner(model_2, 1, 40, verbose=True)
     print(time)
 
-    [MAC, RAM] = model_1.MAC_and_RAM(4)
-    print(f"RAM uncomp_model = {RAM/8/1000/1000}")
+    [MAC, RAM] = model_1.MAC_and_RAM(40,output_in_bytes=True, output_total=False)
+    RAM = [i /(1024**2) if type(i) == int else [j/(1024**2) for j in i] for i in RAM]
+    print(f"RAM uncomp_model = {RAM}")
 
-    [MAC, RAM] = model_2.MAC_and_RAM(4)
-    print(f"RAM CP_model = {RAM/8/1000/1000}")
+    [MAC, RAM] = model_2.MAC_and_RAM(40,output_in_bytes=True, output_total=False)
+    RAM = [i /(1024) if type(i) == int else [j/(1024) for j in i] for i in RAM]
+    print(f"RAM CP_model = {RAM}")
