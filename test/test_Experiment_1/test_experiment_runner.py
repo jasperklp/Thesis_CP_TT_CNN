0150273910bda4294cd7b6ae231a532a7a4fd5dd
jasperklp <jasperklip2@gmail.com>
2024-12-10 14:21:05 +0100

    Updated pytests

--- test/test_Experiment_1/test_experiment_runner.py
+++ test/test_Experiment_1/test_experiment_runner.py
@@ -3,15 +3,20 @@ from ...Experiment_1 import CNN_models
 
 #Do forall tests a simple short run s.t. it can be verified that all functionallity works in the end :)
 def test_for_uncomp_model():
-    model_1 = CNN_models.uncomp_model(256,256,3)
-    runner.model_runner(model_1, 100, 4, verbose=True)
+    model_1 = CNN_models.uncomp_model(16,16,3)
+    runner.model_runner(model_1, 10, 4, verbose=True)
 
 def test_for_cp_model():
-    model_1 = CNN_models.cp_tensorly_model(256,256,3, rank = 0.5)
+    model_1 = CNN_models.cp_tensorly_model(16,16,3, rank = 0.5)
     MAC,RAM = model_1.MAC_and_RAM(4)
-    runner.model_runner(model_1, 100, 4, verbose=True)
+    runner.model_runner(model_1, 10, 4, verbose=True)
 
 def test_for_GIL_model():
-    model_1 = CNN_models.cp_GIL_model(256,256,3, rank = 0.5)
+    model_1 = CNN_models.cp_GIL_model(16,16,3, rank = 0.5)
     MAC,RAM = model_1.MAC_and_RAM(4)
-    runner.model_runner(model_1, 100, 4, verbose=True)
+    runner.model_runner(model_1, 10, 4, verbose=True)
+
+def test_for_tt_model():
+    model_1 = CNN_models.tt_tensorly_model(16,16,3, rank = (1,2,3,4,1))
+    MAC,RAM = model_1.MAC_and_RAM(4)
+    runner.model_runner(model_1,10,4,verbose=True)
