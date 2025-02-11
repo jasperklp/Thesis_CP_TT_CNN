508e17d466dcfc774d60da322fd97465a68d7bfe
jasperklp <jasperklip2@gmail.com>
2025-02-11 14:20:20 +0100

    Update before report

--- Dataprocessing/Experiment_2/data_proc_compare.py
+++ Dataprocessing/Experiment_2/data_proc_compare.py
@@ -3,6 +3,7 @@ import os
 import statistics
 import sys
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 
 #Adds root of thesis folder to sys path.
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
@@ -32,46 +33,54 @@ def main():
     results, model_types = utils.preprocess_time_data(file, folder, var1, var2, "iter_same_in_out", used_models=used_models, used_ranks=used_ranks)
 
     
-    fig,ax = plt.subplots(3,3)
-    used_inchannel = [8,32,256]
-    used_image_size = [8,32,256]
+    fig,ax = plt.subplots(4,4)
+    ax : list[list[Axes]]
+    used_inchannel = [4,16,64,256]
+    used_image_size = [4,16,64,256]
     used_image_size_indices = [measurement_parameters.image_size.index(i) for i in used_image_size]
     # used_inchannel = measurement_parameters.in_channel
 
+    handles = []
 
     for i,_ in enumerate(model_types):
         for j,in_ch in enumerate(used_inchannel):
             for k,size in enumerate(used_image_size):
-                ha_list = ['right' , 'right', 'left' , 'right', 'left']
-                va_list = ['center' , 'center', 'center', 'center', 'center']
-
-                if k==2 or j==2:
-                    ha_list = ['right' , 'right', 'right' , 'left', 'left']
-
-                ax[k][j].scatter(used_inchannel[j],results[0,i,measurement_parameters.image_size.index(in_ch),measurement_parameters.image_size.index(size)] , c=utils.get_mathplotlib_colours(i))
-                ax[k][j].annotate("  " + name_number(int(results[1,i,measurement_parameters.image_size.index(in_ch),measurement_parameters.image_size.index(size)]), always_three_digits=True) + "  " 
-                                  , (used_inchannel[j],results[0,i,measurement_parameters.image_size.index(in_ch),measurement_parameters.image_size.index(size)])
-                                  , ha=ha_list[i], va = va_list[i]
-                )
+                # print(k)
+                handle = ax[k][j].scatter(0.995+i*0.0025,results[0,i,measurement_parameters.image_size.index(in_ch),measurement_parameters.image_size.index(size)]/results[0,0,measurement_parameters.image_size.index(in_ch),measurement_parameters.image_size.index(size)] , c=utils.get_mathplotlib_colours(i))
+                ax[k][j].scatter(1.095+i*0.0025,results[1,i,measurement_parameters.image_size.index(in_ch),measurement_parameters.image_size.index(size)]/results[1,0,measurement_parameters.image_size.index(in_ch),measurement_parameters.image_size.index(size)] , c=utils.get_mathplotlib_colours(i))
                 
+                if k == 0 and j==0:
+                    handles.append(handle)
 
                 # ax[j][2*k+1].scatter(used_inchannel[j],results[1,i,measurement_parameters.image_size.index(in_ch),measurement_parameters.image_size.index(size)] , c=utils.get_mathplotlib_colours(i))
 
 
     for i in range(len(ax) * len(ax[0])):
-        _, top = ax[i//3][i%3].get_ylim()
-        ax[i//3][i%3].set_ylim([0, float(top*1.1)])
+        if i//4  <= 1:
+            ax[i//4][i%4].set_ylim([0, 5])
+        else:
+            ax[i//4][i%4].set_ylim([0, 2.5])
+    
+        ax[i//4][i%4].set_xlim([0.9, 1.2])
+        ax[i//4][i%4].set_xticks([1, 1.1], ["t", "mem"], rotation=0)
 
-    for i in range(3):
-        ax[0][i].set_ylim([0, 0.0017])
+        if i%4 == 0:
+            ax[i//4][i%4].set_ylabel(f"{used_image_size[i//4]} x {used_image_size[i//4]}")
 
-    for i in range(3):
-        ax[1][i].set_ylim([0, 0.0032])
+        if i//4 == 3:
+            ax[i//4][i%4].set_xlabel(f"{used_inchannel[i%4]}")
 
+        
+            
 
-    plt.legend(model_types, loc = 'lower left', bbox_to_anchor = (1.05,0.5),borderaxespad=0.)
+    fig.supylabel("Image size")
+    fig.supxlabel("Input channels = Output channels = ")
+    fig.suptitle("Different inference time and memory ratios\n with the regular CNN as the baseline\nt indicates the time ratio, mem indicates the memory ratio")
+    model_types[0] = utils.uncomp_alternative_name()
+    plt.legend(handles=handles, labels = model_types, loc = 'lower left', bbox_to_anchor = (1.05,0.5),borderaxespad=0.)
     plt.tight_layout(rect=[0,0,0.9,1])
-    fig.subplots_adjust(hspace=0.5, right=0.8)
+    fig.subplots_adjust(hspace=0.5, wspace=0.4, right=0.8)
+    fig.set_size_inches(6.4,6.4)
     plt.show()
 
 if __name__ == "__main__":
--- Dataprocessing/Theoretical_Analysis_in_thesis/ratio.py
+++ Dataprocessing/Theoretical_Analysis_in_thesis/ratio.py
@@ -1,17 +1,51 @@
 import os
 import sys
 import json
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 
 #Adds root of thesis folder to sys path.
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
-from Dataprocessing.dataproc_utils import get_theoretical_results_in_preprocess_measurement_format, uncomp_alternative_name
-from Dataprocessing.Experiment_1.data_proc_experiment_1_system_size import plot_image_size_data_ratio
-from Dataprocessing.Experiment_1.data_proc_experiment_1_kernel_size import plot_image_size_data_ratio as plot_kernel_size_data_ratio
+from Dataprocessing.dataproc_utils import get_theoretical_results_in_preprocess_measurement_format, uncomp_alternative_name, preprocess_measurement_data
+import Dataprocessing.dataproc_utils as utils
+from Experiment_runner.experiment_helper_functions import measurement
+
+
+def plot_image_size_data_ratio_theo_act(results_theo, results_act, measurement_parameters : measurement, model_types):
+    fig,ax = plt.subplots(2,2)
+    ax : list[list[Axes]]
+    # ax = ax[0]
+    for i,item in enumerate(model_types):
+        ax[0][0].scatter([1,2,3,4],results_act[0,i,0,:] / results_theo[0,i,0,:], c=utils.get_mathplotlib_colours(i))
+        ax[0][1].scatter([1,2,3,4],results_act[0,i,1,:] / results_theo[0,i,1,:], c=utils.get_mathplotlib_colours(i))
+        ax[1][0].scatter([1,2,3,4],results_act[0,i,2,:] / results_theo[0,i,2,:], c=utils.get_mathplotlib_colours(i))       
+        ax[1][1].scatter([1,2,3,4],results_act[0,i,3,:] / results_theo[0,i,3,:], c=utils.get_mathplotlib_colours(i))
 
 
 
+    var1 = measurement_parameters.image_size
+    var2 = measurement_parameters.in_channel
 
+    for i,image_size in enumerate(var1):
+        ax[i//2][i%2].axhline(y=1, color='r')
+        ax[i//2][i%2].set_title(f"Image size = {image_size} x {image_size}")
+        # ax[i//2][i%2].set_xscale("log")
+        ax[i//2][i%2].set_ylabel("Ratio")
+        ax[i//2][i%2].set_xlabel("In_channels = out_channels = ")
+        ax[i//2][i%2].set_xticks([1,2,3,4], labels=measurement_parameters.in_channel)
+
+    ax[0][0].set_ylim([0,6]) 
+    ax[0][1].set_ylim([0,6])
+    ax[1][0].set_ylim([0,6]) 
+    ax[1][1].set_ylim([0,6])
+
+    plt.suptitle("Ratio between actual over theoretical amount of memory\n Ratio higher than 1 means actual larger than theoretical")
+    
+    plt.legend(model_types + ["ratio=1"], loc = 'lower left', bbox_to_anchor = (1.05,0.5),borderaxespad=0.)
+    plt.tight_layout(rect=[0,0,0.9,1])
+    fig.subplots_adjust(hspace=0.5, right=0.8)
+    plt.show()
 
 
 
@@ -20,32 +54,32 @@ def system_size():
     folder = "verify_model_matching_ttcp_image_size_default_pytorch"
     read_file = "2025-01-21_16.26.18"
 
-    results, measument_parameters, model_types = get_theoretical_results_in_preprocess_measurement_format(read_file, folder, "same_in_out", "image_size", "in_channel", used_ranks=[0.01,0.05,0.1,0.25,1.0] )    
+    results_theory, _, model_types = get_theoretical_results_in_preprocess_measurement_format(read_file, folder, "same_in_out", "image_size", "in_channel", used_models = ["uncomp" , "cp" , "tt"],used_ranks=[0.01,0.05,0.1,0.25,1.0] )    
+    results_actual, measument_parameters, model_types =  preprocess_measurement_data(read_file,folder, "image_size", "in_channel", used_models = ["uncomp" , "cp" , "tt"],used_ranks=[0.01,0.05,0.1,0.25,1.0])
 
     # print(measument_parameters)
     # print(model_types)
     # print(results)
-    print(model_types)
     model_types[0] = uncomp_alternative_name()
-    print(model_types)
-    plot_image_size_data_ratio(results, measument_parameters, model_types )
+    plot_image_size_data_ratio_theo_act(results_theory, results_actual, measument_parameters, model_types )
 
-def kernel_size():
+# def kernel_size():
 
-    folder = "verify_model_matching_ttcp_kernel_size_default_pytorch"
-    read_file = "2025-01-21_16.27.13"
+#     folder = "verify_model_matching_ttcp_kernel_size_default_pytorch"
+#     read_file = "2025-01-21_16.27.13"
 
-    results, measument_parameters, model_types = get_theoretical_results_in_preprocess_measurement_format(read_file, folder, "same_in_out_same_kernel_pad", "kernel_size", "in_channel", used_ranks=[0.01,0.05,0.1,0.25,1.0] )    
+#     results, measument_parameters, model_types = get_theoretical_results_in_preprocess_measurement_format(read_file, folder, "same_in_out_same_kernel_pad", "kernel_size", "in_channel", used_ranks=[0.01,0.05,0.1,0.25,1.0] )    
 
-    # print(measument_parameters)
-    # print(model_types)
-    # print(results)
-    print(model_types)
-    model_types[0] = uncomp_alternative_name()
-    print(model_types)
-    plot_kernel_size_data_ratio(results, measument_parameters, model_types )
+#     # print(measument_parameters)
+#     # print(model_types)
+#     # print(results)
+#     print(model_types)
+#     model_types[0] = uncomp_alternative_name()
+#     print(model_types)
+#     plot_kernel_size_data_ratio(results, measument_parameters, model_types )
 
 
 if __name__ == "__main__":
     system_size()
     # kernel_size()
+
